1cd774199e0b4f09b9f559c26122f0448b94d988
Philip Schlump <pschlump@gmail.com>
2016-06-20 21:10:53 -0600

    added check for function to see if is UUID

---
+++ is_uuid.go
@@ -0,0 +1,28 @@
+// This package provides immutable UUID structs and the functions
+// NewV3, NewV4, NewV5 and Parse() for generating versions 3, 4
+// and 5 UUIDs as specified in RFC 4122.
+//
+// Copyright (C) 2011 by Krzysztof Kowalik <chris@nu7hat.ch>
+// Copyright (C) 2012-2015 by Philip Schlump - MIT License
+
+package uuid
+
+import "regexp"
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+const hexUUIDPattern = "^([a-z0-9]{8})-([a-z0-9]{4})-([1-5][a-z0-9]{3})-([a-z0-9]{4})-([a-z0-9]{12})$"
+
+var uuidPattern *regexp.Regexp
+
+func init() {
+	uuidPattern = regexp.MustCompile(hexUUIDPattern)
+}
+
+func IsUUID(s string) bool {
+	md := uuidPattern.FindStringSubmatch(s)
+	if md == nil {
+		return false
+	}
+	return true
+}
